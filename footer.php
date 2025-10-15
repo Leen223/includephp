77f05b84531dd52eba475a2e6c1bbea485bef43d
Leen <leenalfaleet2234@gmail.com>
2025-10-16 02:58:10 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class='footer'>
+    <p>&copy; <?php echo date("Y");?> My php website all rights reserved.</p>
+   
+</div>
---
+++ header.php
@@ -0,0 +1,8 @@
+<div class='header'>
+    <img src='images/header.png' alt= 'header image' class='header-image'>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+
+</nav>
+</div>
